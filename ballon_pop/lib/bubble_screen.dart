a4fee38c863f61aba7ae1e9afbb0fd462642a376
siddhardh-7 <cs20b1063@iiitdm.ac.in>
2024-03-20 23:08:25 +0530

    Minor Changes

--- ballon_pop/lib/bubble_screen.dart
+++ ballon_pop/lib/bubble_screen.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 import 'dart:math';
 import 'dart:ui';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 import 'balloon.dart';
 
 class BubbleScreen extends StatefulWidget {
@@ -14,16 +17,15 @@ class BubbleScreen extends StatefulWidget {
 class _BubbleScreenState extends State<BubbleScreen>
     with WidgetsBindingObserver {
   var colorsList = <Color>[
-    Colors.red,
-    Colors.blue,
-    Colors.green,
-    Colors.yellow,
-    Colors.purple,
+    Color(0xFF280C3D),
+    Color(0xFFFD9B4A),
+    Color(0xFF3A7861),
+    Color(0xFFF9CE34),
+    Color(0xFF6A54B8),
     Colors.orange,
-    Colors.pink,
-    Colors.teal,
-    Colors.brown,
-    Colors.grey,
+    Color(0xFFB16648),
+    Color(0xFF8E511D),
+    Color(0xFFB3B3B3),
   ];
   int totalBolloons = 0;
   late Timer timer;
@@ -34,6 +36,8 @@ class _BubbleScreenState extends State<BubbleScreen>
   int score = 0;
   late Size size;
   bool start = false;
+  int _start = 120;
+  String _remainingTime = '2:00';
 
   @override
   void initState() {
@@ -61,9 +65,6 @@ class _BubbleScreenState extends State<BubbleScreen>
       timer.cancel();
     }
   }
-
-  int _start = 120;
-  String _remainingTime = '2:00';
 
   void startGame() {
     timer = Timer.periodic(Duration(seconds: 1), (timer) {
@@ -103,17 +104,22 @@ class _BubbleScreenState extends State<BubbleScreen>
     setState(() {
       start = true;
       totalBolloons = bubbles.length;
+      _remainingTime = '2:00';
+      _start = 120;
       bubbles.clear();
     });
   }
 
   void restartGame() {
-    totalBolloons = bubbles.length;
-    bubbles.clear();
-    score = 0;
-    start = false;
-    startGame();
-    setState(() {});
+    setState(() {
+      totalBolloons = bubbles.length;
+      _remainingTime = '2:00';
+      _start = 120;
+      bubbles.clear();
+      score = 0;
+      start = false;
+      startGame();
+    });
   }
 
   @override
@@ -123,105 +129,154 @@ class _BubbleScreenState extends State<BubbleScreen>
     return Scaffold(
       body: !start
           ? Stack(children: [
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/background.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
               for (int i = 0; i < bubbles.length; i++) bubbles[i],
             ])
           : Container(
-        margin: EdgeInsets.symmetric(horizontal: 50, vertical: 220),
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: Colors.amber,
-        ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children:[
-                Text(
-                  'Score Board',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,),
-                ),
-                Divider(
-                  thickness:2,
-                  color: Colors.white,
-                ),
-                Text(
-                  'Balloons Popped: $score',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,),
-                ),
-                Text(
-                  'Balloons Missed: ${totalBolloons - score}',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                ),
-                Text(
-                  'You Score',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white,
-                  ),
-                  child: Text(
-                    '${score*2 - (totalBolloons-score)}',
+              margin: EdgeInsets.symmetric(horizontal: 50, vertical: 220),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.lightGreen,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    'Score Board',
                     textAlign: TextAlign.center,
                     style: const TextStyle(
-                        fontSize: 32,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Divider(
+                    thickness: 2,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    'Balloons Popped: $score',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Text(
+                    'Balloons Missed: ${totalBolloons - score}',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontSize: 18,
                         fontWeight: FontWeight.bold,
                         color: Colors.black),
                   ),
+                  Text(
+                    'You Score',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                    ),
+                    child: Text(
+                      '${score * 2 - (totalBolloons - score)}',
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: SafeArea(
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Visibility (
+                visible: !start,
+                child: Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.all(50),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.white,
+                  ),
+                  child: Text(
+                    "Score: ${3 * score - bubbles.length}",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+
+                    ),
+                  ),
                 ),
-              ],
+              ),
             ),
-          ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Container(
-        width: double.infinity,
-        padding: const EdgeInsets.symmetric(vertical: 12),
-        margin: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color.fromRGBO(255,94,247,1), Color.fromRGBO(2,245,255,1)],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-          borderRadius: BorderRadius.circular(50),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black26,
-              blurRadius: 5,
-              offset: Offset(0, 3),
-            )
+            Expanded(child: Container()),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(vertical: 12),
+                margin: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Color(0xFF3A7861),
+                  // gradient: LinearGradient(
+                  //   colors: [
+                  //     Color.fromRGBO(255, 94, 247, 1),
+                  //     Color.fromRGBO(2, 245, 255, 1)
+                  //   ],
+                  //   begin: Alignment.topLeft,
+                  //   end: Alignment.bottomRight,
+                  // ),
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    )
+                  ],
+                ),
+                child: TextButton(
+                  onPressed: () {
+                    if (start) {
+                      restartGame();
+                    }
+                  },
+                  child: Text(
+                    start ? 'Restart' : 'Remaining Time: $_remainingTime',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
-        ),
-        child: TextButton(
-          onPressed: () {
-            if (start) {
-              restartGame();
-            }
-          },
-          child: Text(
-            start ? 'Restart' : 'Remaining Time: $_remainingTime',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-            ),
-          ),
         ),
       ),
     );
